0e5b0e446e121f15db834bc3035131e23f13a2a1
illyakich <speedfeed123@gmail.com>
2024-10-04 22:04:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+Console.WriteLine("give me a number between 1 and 40");
+int input = Convert.ToInt32(Console.ReadLine());
+AskForNumberInRange(input, 1, 40);
+int AskForNumberInRange(int Number, int min, int max)
+{
+   while (true)
+    {
+        if (Number > min & Number < max)
+            return Number;
+
+        else
+        {
+            Console.WriteLine("Try again");
+        }
+    }
+}
+Console.WriteLine("nice");
