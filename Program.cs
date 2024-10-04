0afed2f693f869e392aa74b5535684781ae1fa2a
illyakich <speedfeed123@gmail.com>
2024-10-04 23:08:42 +0300

    this is an example where u can use the methods.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
-﻿int AskForNumber(string text)
+﻿int guess = AskForNumberInRange("user 1, type a number between 0 to 100", 0, 100);
+
+Console.Clear();
+
+while (true)
+{
+    int userguess = AskForNumber("whats your next guess?");
+    if (userguess < guess)
+    {
+        Console.WriteLine("seriously?");
+    }
+    else if (userguess > guess)
+    {
+        Console.WriteLine("too much");
+    }
+    else break;
+}
+Console.WriteLine("nice");
+
+int AskForNumber(string text)
 {
     Console.WriteLine(text);
     int result = Convert.ToInt32(Console.ReadLine());
