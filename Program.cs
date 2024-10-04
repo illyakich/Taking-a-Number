9c5ab6230f6620424a0ae034b0e20858fd87de8a
illyakich <speedfeed123@gmail.com>
2024-10-04 22:53:06 +0300

    actual solution

--- Program.cs
+++ Program.cs
@@ -4,20 +4,14 @@
     int result = Convert.ToInt32(Console.ReadLine());
     return result;
 }
-Console.WriteLine("give me a number between 1 and 40");
-int input = Convert.ToInt32(Console.ReadLine());
-AskForNumberInRange(input, 1, 40);
-int AskForNumberInRange(int Number, int min, int max)
-{
-   while (true)
-    {
-        if (Number > min & Number < max)
-            return Number;
 
-        else
-        {
-            Console.WriteLine("Try again");
-        }
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int ask = AskForNumber(text);
+        if (ask < min || ask > max)
+                Console.WriteLine("try again");
+        else return ask;
     }
 }
-Console.WriteLine("nice");
